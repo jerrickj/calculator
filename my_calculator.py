41988da50bc4b0f6e72da5047aca868aed0df9b6
jerrickj <jerrickj96@gmail.com>
2021-06-18 17:09:32 -0400

    Update my_calculator.py

--- my_calculator.py
+++ my_calculator.py
@@ -44,6 +44,7 @@ def button_decimal():
 
 #Functions for math
 def button_clear():
+    output["state"] = NORMAL
     output.delete(0, END)
 
 def button_add():
@@ -87,7 +88,11 @@ def button_equals():
     elif input_symbol == "*":
         output.insert(0, float(input_1) * float(input_2))
     elif input_symbol == "/":
-        output.insert(0, float(input_1) / float(input_2))  
+        if input_2 == 0 or input_2 == "0":
+            output.insert(0, "Error, divide by 0")
+            output["state"] = DISABLED
+        else:
+            output.insert(0, float(input_1) / float(input_2))  
 
 #define number buttons
 button_1 = Button(root, text = "1", borderwidth= 2, border = 8, font = myFont, activebackground = "Blue", command = lambda: button_click(1))
@@ -107,10 +112,9 @@ add =      Button(root, text = "+", borderwidth= 2, border = 8, font = myFont, a
 subtract = Button(root, text = "-", borderwidth= 2, border = 8, font = myFont, activebackground = "Purple", command = button_subtract)
 multiply = Button(root, text = "*", borderwidth= 2, border = 8, font = myFont, activebackground = "Purple", command = button_multiply)
 divide =   Button(root, text = "/", borderwidth= 2, border = 8, font = myFont, activebackground = "Purple", command = button_divide)
+decimal =  Button(root, text = ".", borderwidth= 2, border = 8, font = myFont, activebackground = "Purple", command = button_decimal)
 clear =    Button(root, text = "C", borderwidth= 2, border = 8, font = myFont, activebackground = "Red", command = button_clear)
 equals =   Button(root, text = "=", borderwidth= 2, border = 8, font = myFont, activebackground = "Red", command = button_equals)
-decimal =  Button(root, text = ".", borderwidth= 2, border = 8, font = myFont, activebackground = "Purple", command = button_decimal)
-
 
 #Put number buttons on grid on screen
 button_1.grid(row =3, column =0, sticky=NSEW)
@@ -129,58 +133,9 @@ add.grid(row = 1, column = 3, sticky=NSEW)
 subtract.grid(row = 2, column = 3, sticky=NSEW)
 multiply.grid(row = 3, column = 3, sticky=NSEW)
 divide.grid(row = 4, column = 3, sticky=NSEW)
+decimal.grid(row = 4, column = 1, sticky=NSEW)
 clear.grid(row = 4, column = 2, sticky=NSEW)
 equals.grid(row = 5, column = 0, columnspan= 4, sticky=NSEW)
-decimal.grid(row = 4, column = 1, sticky=NSEW)
 
 #end master window
 root.mainloop()
-
-#testing/learning code not needed for program
-
-#create list of all buttons
-#button_list = [button_1, button_2, button_3, button_4, button_5, button_6, button_7, button_8, button_9, button_0, add, subtract, multiply, divide, clear, equals, decimal]
-
-#loop for configuring all buttons to dynamically resize
-#number_of_rows = 5
-#number_of_columns = 5
-#column_counter = 0
-#row_counter = 0
-#for button in button_list:
-#    if column_counter < number_of_columns:
-#        Grid.columnconfigure(root, index = column_counter, weight = 1)
-#    if row_counter < number_of_rows:
-#        Grid.rowconfigure(root, index = row_counter, weight = 1)
-#    column_counter += 1
-#    row_counter += 1
-
-
-#improved (simpler) version of above loop with similar result
-#column_counter = 0
-#row_counter = 0
-#for x in range(0,5):
-#    Grid.columnconfigure(root, index = column_counter, weight = 1)
-#    Grid.rowconfigure(root, index = row_counter, weight = 1)
-#    column_counter += 1
-#    row_counter += 1
-
-#pads for number selector buttons
-#padx = 20, pady = 20, 
-#padx = 20, pady = 20, 
-#padx = 20, pady = 20, 
-#padx = 20, pady = 20, 
-#padx = 20, pady = 20, 
-#padx = 20, pady = 20, 
-#padx = 20, pady = 20, 
-#padx = 20, pady = 20, 
-#padx = 20, pady = 20, 
-#padx = 20, pady = 20, 
-
-#pads for math selector buttons
-#padx = 19, pady = 20,
-#padx = 21, pady = 20,
-#padx = 21, pady = 20,
-#padx = 21, pady = 20,
-#padx = 20, pady = 20,
-#padx = 19, pady = 20,
-#padx = 21, pady = 20,
